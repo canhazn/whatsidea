aa8e0d12554f71018294523569d68f105af5b350
loc.luu <loc.luu@bluecoglobal.com>
2020-12-02 11:39:50 +0700

    loc like post

--- like/views.py
+++ like/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import get_object_or_404
 from core import models
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.http import JsonResponse
 import json
 
 
 @login_required
 def like(request):
     data = json.loads(request.body)
-    if data["post_id"]:
+    print(data)
+    if "post_id" in data:
         post_id = data["post_id"]
         post = get_object_or_404(models.Post, id=post_id)
         like, created_post = models.Like.objects.get_or_create(user=request.user, post=post,)
@@ -22,7 +22,7 @@ def like(request):
             return JsonResponse({
                 "message": "like created"
             })
-    elif data["comment_id"]:
+    elif "comment_id" in data:
         comment_id = data["comment_id"]
         comment = get_object_or_404(models.Comment, id=comment_id)
         like, created_comment = models.Like.objects.get_or_create(user=request.user, comment=comment)
@@ -36,4 +36,6 @@ def like(request):
                 "message": "like created"
             })
 
-    
+    return JsonResponse({
+                "message": "like created"
+            })
