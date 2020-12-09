b5fdef76574b4263a4ab221fb7782e677ee972ab
canhazn <canhazn@gmail.com>
2020-12-09 17:44:14 +0700

    update css

--- whatsidea/templatetags/load_parent_comment.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='load_parent_comment')
-def load_parent_comment(comment):
-    return comment.filter(parent=None)
