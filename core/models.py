e6d77bd8f6aae7beb4c1a0d0cb9c7f6fdd5c8876
canhazn <canhazn@gmail.com>
2020-12-18 15:42:49 +0700

    post create by editor

--- core/models.py
+++ core/models.py
@@ -74,7 +74,7 @@ class Post(models.Model):
         return str(self.content)
 
     class Meta:
-        ordering = ["date_created"]
+        ordering = ["-date_created"]
 
 
 class Contribution(models.Model):
